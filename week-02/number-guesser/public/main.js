1e8ef2b6b018722b6a5418057bafbbc20db111f0
Mark Dewey <mark.t.dewey@gmail.com>
2019-08-06 11:26:44 -0400

    added logic for correct guess, added dynamic css

--- week-02/number-guesser/public/main.js
+++ week-02/number-guesser/public/main.js
@@ -1,6 +1,28 @@
+let targetNumber = 0
+
 const main = () => {
-  const targetNumber = Math.ceil(Math.random() * 10)
-  console.log(targetNumber)
+  targetNumber = Math.ceil(Math.random() * 10)
+  console.log('that target number is : ', targetNumber)
+}
+
+const checkGuess = () => {
+  //get the user's guess
+  const userGuess = document.querySelector('#numberGuessed').value
+  console.log(userGuess)
+  // compare the user guess and target number
+  if (parseInt(userGuess) === targetNumber) {
+    // display yes!
+    console.log('matched!')
+    document.querySelector('.result').textContent = 'Yes! Correct! You Win!'
+    document.querySelector('.result').classList.remove('incorrect-guess')
+    document.querySelector('.result').classList.add('correct-guess')
+  } else {
+    // display no!
+    console.log('no match')
+    document.querySelector('.result').textContent = 'Nope! Try again!'
+    document.querySelector('.result').classList.add('incorrect-guess')
+  }
 }
 
 document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.do-guess').addEventListener('click', checkGuess)
