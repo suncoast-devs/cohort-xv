6f270214bd8700a31bda5e5520e36eacaf822d92
Mark Dewey <mark.t.dewey@gmail.com>
2019-08-12 12:37:14 -0400

    refactored to use map/filter/reduce

--- week-03/deck-shuffler/public/main.js
+++ week-03/deck-shuffler/public/main.js
@@ -1,37 +1,33 @@
-const state = {
-  suits: ['hearts', 'diamonds', 'spades', 'clubs'],
-  ranks: [
-    { name: 'ace', value: 11 },
-    { name: '2', value: 2 },
-    { name: '3', value: 3 },
-    { name: '4', value: 4 },
-    { name: '5', value: 5 },
-    { name: '6', value: 6 },
-    { name: '7', value: 7 },
-    { name: '8', value: 8 },
-    { name: '9', value: 9 },
-    { name: '10', value: 10 },
-    { name: 'jack', value: 10 },
-    { name: 'queen', value: 10 },
-    { name: 'king', value: 10 }
-  ],
+const suits = ['hearts', 'diamonds', 'spades', 'clubs']
+const ranks = [
+  { name: 'ace', value: 11 },
+  { name: '2', value: 2 },
+  { name: '3', value: 3 },
+  { name: '4', value: 4 },
+  { name: '5', value: 5 },
+  { name: '6', value: 6 },
+  { name: '7', value: 7 },
+  { name: '8', value: 8 },
+  { name: '9', value: 9 },
+  { name: '10', value: 10 },
+  { name: 'jack', value: 10 },
+  { name: 'queen', value: 10 },
+  { name: 'king', value: 10 }
+]
 
-  deck: [],
-  playerHand: []
-}
+let deck = []
+const playerHand = []
 
 const createDeck = () => {
-  for (let i = 0; i < state.suits.length; i++) {
-    const suit = suits[i]
-    for (let j = 0; j < state.ranks.length; j++) {
-      const rank = ranks[j]
+  suits.forEach(suit => {
+    ranks.forEach(rank => {
       deck.push({
         rank: rank.name,
         value: rank.value,
         suit: suit
       })
-    }
-  }
+    })
+  })
   console.log(deck)
 }
 const shuffleDeck = () => {
@@ -54,18 +50,42 @@ const dealPlayerHand = () => {
   console.log(playerHand)
 }
 const getPlayerHandTotal = () => {
-  let handTotal
-  for (let i = 0; i < playerHand.length; i++) {
-    const card = playerHand[i]
-    // add the current card value to a hand total
-    if (handTotal) {
-      handTotal += card.value
-    } else {
-      handTotal = card.value
-    }
-  }
-  // display the total
+  // let handTotal
+  // for (let i = 0; i < playerHand.length; i++) {
+  //   const card = playerHand[i]
+  //   // add the current card value to a hand total
+  //   if (handTotal) {
+  //     handTotal += card.value
+  //   } else {
+  //     handTotal = card.value
+  //   }
+  // }
+  // // display the total
+  const handTotal = playerHand.reduce((runningTotal, card) => {
+    return runningTotal + card.value
+  }, 0)
   console.log(handTotal)
+}
+
+const displayPlayerCards = () => {
+  // foreach card create a new li
+  playerHand
+    .map(card => {
+      const unicorn = document.createElement('li')
+      unicorn.textContent = card.rank + ' of ' + card.suit
+      return unicorn
+    })
+    .forEach(unicorn => {
+      document.querySelector('.player-hand').appendChild(unicorn)
+    })
+  // foreach li append to the DOM
+}
+
+const filterOutAces = () => {
+  console.log(deck)
+  deck = deck.filter(card => {
+    return card.rank !== 'ace'
+  })
 }
 
 const basicForLoop = () => {
@@ -76,11 +96,13 @@ const basicForLoop = () => {
 }
 
 const main = () => {
-  // createDeck()
-  // shuffleDeck()
-  // dealPlayerHand()
-  // getPlayerHandTotal()
-  basicForLoop()
+  createDeck()
+  shuffleDeck()
+  filterOutAces()
+  dealPlayerHand()
+  getPlayerHandTotal()
+  displayPlayerCards()
+  // basicForLoop()
 }
 
 document.addEventListener('DOMContentLoaded', main)
