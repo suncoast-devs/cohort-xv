163acf509942caae61be9df7d9e300c5ab106241
Mark Dewey <mark.t.dewey@gmail.com>
2019-08-13 11:11:44 -0400

    added fetch

--- week-03/joke-api/public/main.js
+++ week-03/joke-api/public/main.js
@@ -1,7 +1,19 @@
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  // we want to call the API, using fetch
+  // console.log the response
+  fetch('https://official-joke-api.appspot.com/random_joke')
+    .then(response => {
+      console.log(response)
+      return response.json()
+    })
+    .then(joke => {
+      console.log(joke)
+      document.querySelector('.setup').textContent = joke.setup
+      document.querySelector('.punchline').textContent = joke.punchline
+    })
 }
 
 document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.reveal-punchline').addEventListener('click', () => {
+  document.querySelector('.punchline').classList.remove('hide')
+})
