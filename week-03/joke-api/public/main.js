7325888ddb88755281e39830b0bb45cfe971f908
Mark Dewey <mark.t.dewey@gmail.com>
2019-08-13 11:17:24 -0400

    added reset button

--- week-03/joke-api/public/main.js
+++ week-03/joke-api/public/main.js
@@ -1,6 +1,9 @@
 const main = () => {
   // we want to call the API, using fetch
   // console.log the response
+  document.querySelector('.punchline').classList.add('hide')
+  document.querySelector('.setup').textContent = '...'
+
   fetch('https://official-joke-api.appspot.com/random_joke')
     .then(response => {
       console.log(response)
@@ -17,3 +20,5 @@ document.addEventListener('DOMContentLoaded', main)
 document.querySelector('.reveal-punchline').addEventListener('click', () => {
   document.querySelector('.punchline').classList.remove('hide')
 })
+
+document.querySelector('.get-another').addEventListener('click', main)
