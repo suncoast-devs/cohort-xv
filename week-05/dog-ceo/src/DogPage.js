0cf8d3efa131eb2c4ecd214b96978a23ed1e0aa1
Mark Dewey <mark.t.dewey@gmail.com>
2019-08-28 11:18:00 -0400

    added searching

--- week-05/dog-ceo/src/DogPage.js
+++ week-05/dog-ceo/src/DogPage.js
@@ -1,30 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const DogPage = () => {
+  const [dogs, setDogs] = useState([])
+  const [searchTerm, setSearchTerm] = useState('hound')
+
+  const fetchData = async () => {
+    const resp = await axios.get(
+      `https://dog.ceo/api/breed/${searchTerm}/images`
+    )
+    console.log(resp.data)
+    setDogs(resp.data.message.slice(0, 10))
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <>
       <main>
         <section>
-          <input type="search" className="search-box" />
-          <button className="search-button">
-            <i className="fas fa-search fa-flip-horizontal "></i>
+          <input
+            type="search"
+            className="search-box"
+            value={searchTerm}
+            onChange={e => setSearchTerm(e.target.value)}
+          />
+          <button className="search-button" onClick={fetchData}>
+            <i className="fas fa-search fa-flip-horizontal "></i>SEARCH
           </button>
         </section>
         <section>
           <ul>
-            <li>DOG PICTURES</li>
-            <li>DOG PICTURES</li>
-            <li>DOG PICTURES</li>
-            <li>DOG PICTURES</li>
-            <li>DOG PICTURES</li>
-            <li>DOG PICTURES</li>
-            <li>DOG PICTURES</li>
-            <li>DOG PICTURES</li>
-            <li>DOG PICTURES</li>
-            <li>DOG PICTURES</li>
-            <li>DOG PICTURES</li>
-            <li>DOG PICTURES</li>
-            <li>DOG PICTURES</li>
+            {dogs.map((dogImage, i) => {
+              return (
+                <li key={i}>
+                  <img src={dogImage} />
+                </li>
+              )
+            })}
           </ul>
         </section>
       </main>
