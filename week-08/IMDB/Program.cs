fc5b75506855b65c95f5f88bafa6e05628110e59
Mark Dewey <mark.t.dewey@gmail.com>
2019-09-16 11:42:25 -0400

    Simple Queries

--- week-08/IMDB/Program.cs
+++ week-08/IMDB/Program.cs
@@ -1,12 +1,77 @@
 ﻿using System;
+using System.Linq;
 
 namespace IMDB
 {
-    class Program
+  class Program
+  {
+
+    static void CreateData()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      // create a bunch of movies 
+      // 1. Reference the database
+      var db = new MovieContext();
+      // 2. Do the thing
+      // create a new movie
+      var favoriteMovie = new Movie
+      {
+        Name = "Jaws"
+      };
+      db.Movies.Add(favoriteMovie);
+      // 3. Save the thing
+      db.SaveChanges();
+
+
     }
+
+
+    static void ReadData()
+    {
+      // 1. Reference the database
+      var db = new MovieContext();
+      // 2. Do the thing you wanna do
+      var allTheTitles = db.Movies
+        .Where(movie => movie.ReleasedDate.Year == 1988)
+        .Select(movie => movie.Name);
+      /*
+        a whole bunch of stuff here.....
+      
+       */
+      Console.WriteLine(allTheTitles.First());
+
+
+    }
+    static void UpdateData()
+    {
+
+      // 1. Reference the database
+      var db = new MovieContext();
+      // 2. find the thing you want to update
+      var movieToUpdate = db.Movies.FirstOrDefault(movie => movie.Name == "Jaws");
+      // 3. Update the thing
+      movieToUpdate.Tagline = "You're gonna need a bigger boat";
+      movieToUpdate.Budget = 100000;
+      // 4. Save the updates
+      db.SaveChanges();
+
+    }
+    static void DeleteData()
+    {
+      // 1. Reference the database
+      var db = new MovieContext();
+      // 2. find the the thing
+      var movieToDelete = db.Movies.FirstOrDefault(movie => movie.Name == "Jaws");
+      // 3. Delete the thing
+      db.Movies.Remove(movieToDelete);
+      // 4. Save the changes
+      db.SaveChanges();
+    }
+
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      CreateData();
+    }
+  }
 }
