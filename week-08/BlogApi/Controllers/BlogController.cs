d48bf15a53efbad74ee767539edf0956fc58a40b
Mark Dewey <mark.t.dewey@gmail.com>
2019-09-18 11:20:26 -0400

    Added Get all blogs

--- week-08/BlogApi/Controllers/BlogController.cs
+++ week-08/BlogApi/Controllers/BlogController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using blogapi;
 using BlogApi.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -22,5 +24,18 @@ namespace BlogApi.Controllers
       return entry;
 
     }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Blog>> GetAllBlogs()
+    {
+      // 1. reference the database 
+      var context = new DatabaseContext();
+      // 2. do the thing
+      var blogs = context.Blogs.OrderByDescending(blog => blog.DateCreated);
+
+      // 3. return the thing
+      return blogs.ToList();
+    }
+
   }
 }
